83aa2a5a9bcb71486d6f562853488caaf46af2d7
Craig Markowitz <cmarkowitz881970@gmail.com>
2026-05-27 08:01:17 -0400

    Initial Commit

---
+++ music-iq-claude-2026/music_iq_claude_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  music_iq_claude_2026App.swift
+//  music-iq-claude-2026
+//
+//  Created by Craig Markowitz on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct music_iq_claude_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
